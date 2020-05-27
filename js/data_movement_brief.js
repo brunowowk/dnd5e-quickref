b180c3eb30088d40e164ec32ebad4a6b67471548
OrbitalBliss <orbitalb@hotmail.com>
2020-05-26 20:14:36 -0400

    Added Brief Version of QuickRef

---
+++ js/data_movement_brief.js
@@ -0,0 +1,40 @@
+data_movement_brief = [
+    {
+        title: "Move",
+        icon: "walk",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        reference: "PHB, pg. 190.",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Difficult terrain & Other creatures",
+        icon: "stone-pile",
+        subtitle: "Cost modifier: +5ft per 5ft",
+        reference: "PHB, pg. 182.",
+        description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        bullets: [
+            "This rule is true even if multiple things in a space count as difficult terrain. Low furniture, rubble, undergrowth, steep stairs, snow, and shallow bogs are examples of difficult terrain.",
+            "You can move through a nonhostile creature's space. In contrast, you can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Whether a creature is a friend or an enemy, its space counts as difficult terrain, and you can't willingly end your move in its space.",
+            "If you leave a hostile creature's reach during your move, you provoke an opportunity attack.",
+            "Crawling, Swimming, Climbing are other forms of movement that work similarily to difficult terrain (but don't count as difficult terrain), each adding an extra 5ft to 5ft of movement."
+        ]
+    },
+    {
+            title: "Stand up",
+            icon: "strong",
+            subtitle: "Cost: half of speed",
+            description: "Movement cost: half of your speed",
+            reference: "PHB, pg. 190-191.",
+            bullets: [
+                "You can't stand up if you don't have enough movement left or if your speed is 0"
+            ]
+    }
+]
